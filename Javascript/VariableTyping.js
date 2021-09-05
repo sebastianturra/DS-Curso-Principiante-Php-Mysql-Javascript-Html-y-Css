76e5dcbd3ecdb2b30d4852095fd63dbbb8833887
Sebastian <sebastianturra@gmail.com>
2021-09-05 16:41:10 -0300

    Subida 05.09.2021.16.41

---
+++ Javascript/VariableTyping.js
@@ -0,0 +1,20 @@
+alert('Ganando con mi cariño');
+/*
+x=10;
+document.write("<br>El valor original es: "+x+"<br>");
+x+=10;
+document.write("<br>El valor sumado es: "+x+"<br>");
+*/
+/*
+greeting = "Hello there";
+greeting= "\'Hello there\' is a greeting";
+document.write(greeting);
+*/
+/*
+toys = ['bat','ball','whistle','puzzle','doll'];
+top = ['A', 'B', 'C'];
+midd = ['D', 'E', 'F'];
+bottom = ['G', 'H', 'I'];
+face = [ top, midd, bottom];
+document.write(face[1][2]);
+*/
---
+++ index.php
@@ -0,0 +1,9 @@
+<!DOCTYPE HTML>
+<html>
+<head> 
+    <script type="text/javascript" src="Javascript/VariableTyping.js"></script>
+</head>
+<body> 
+ 
+</body>
+</html>
